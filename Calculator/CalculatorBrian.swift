e8b3fd19bd5bd215e06940a6a0f628721be66237
s20171103186 <s20171103186@admindeiMac-34.local>
2018-11-02 21:10:00 +0800

    计算器
    简单计算器

---
+++ Calculator/CalculatorBrian.swift
@@ -0,0 +1,81 @@
+//
+//  CalculatorBrian.swift
+//  Calculator
+//
+//  Created by s20171103186 on 2018/10/26.
+//  Copyright © 2018 s20171103186. All rights reserved.
+//
+
+import Foundation
+
+
+struct CalculatorBrain {
+    private var accumulator: Double?
+    
+    private enum Operation{
+        case constant(Double)         //常量
+        case unaryOperation((Double)-> Double)   //一元计算
+        case binaryOperation((Double,Double) -> Double)    //二元计算
+        case equels
+    }
+    
+    private var operations: Dictionary<String,Operation> = [
+        "√" : Operation.unaryOperation(sqrt),
+        "cos" : Operation.unaryOperation(cos),
+        "sin" : Operation.unaryOperation(sin),
+        "∓" : Operation.unaryOperation({ -$0 }),
+        "×" : Operation.binaryOperation({ $0 * $1}),
+        "÷" : Operation.binaryOperation({ $0 / $1}),
+        "+" : Operation.binaryOperation({ $0 + $1}),
+        "-" : Operation.binaryOperation({ $0 - $1}),
+        "=" : Operation.equels
+    ]
+    mutating func performOperation(_ symbol: String )  {
+        if let operation = operations[symbol]{
+            switch operation{
+            case.constant(let value):
+                accumulator = value
+            case.unaryOperation(let function):
+                if accumulator != nil{
+                    accumulator = function(accumulator!)
+                }
+            case.binaryOperation(let function):
+                if accumulator != nil {
+                    pendingBinaryOperation = PendingBinaryOperand(function: function, firstOperand: accumulator!)
+                    accumulator = nil
+                }
+            case.equels:
+                performPendingBinaryOperation()
+            }
+        }
+    }
+    
+    private mutating func performPendingBinaryOperation() {
+        if pendingBinaryOperation != nil && accumulator != nil {
+            accumulator = pendingBinaryOperation!.perform(with: accumulator!)
+            pendingBinaryOperation = nil
+        }
+    }
+    
+    private var pendingBinaryOperation: PendingBinaryOperand?
+    
+    private struct PendingBinaryOperand {
+        let function: (Double,Double) -> Double
+        let firstOperand: Double
+        
+        func perform(with secondOperand: Double) -> Double {
+            return function(firstOperand,secondOperand)
+        }
+    }
+    mutating func setOperand(_ operand: Double) {
+        accumulator = operand
+    }
+    
+    
+    var result: Double? {
+        get{
+            return accumulator
+        }
+    }
+}
+
--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,6 +10,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var display: UILabel!
+    var userIsInTheMiddleOfTyping = false
+    @IBAction func touchDigit(_ sender: UIButton) {
+        let digit = sender.currentTitle!
+        if userIsInTheMiddleOfTyping{
+            let textCurrentlyInDisplay = display.text!
+            display.text = textCurrentlyInDisplay + digit
+        }else{
+            display.text = digit
+            userIsInTheMiddleOfTyping = true
+        }
+        
+    }
+    
+    var displayValue:Double{
+        get{
+            return Double(display.text!)!
+        }
+        set{
+            display.text = String(newValue)
+        }
+    }
+    
+    private var brain:CalculatorBrain = CalculatorBrain()
+    
+    @IBAction func performOperation(_ sender: UIButton) {
+        if userIsInTheMiddleOfTyping {
+            brain.setOperand(displayValue)
+            userIsInTheMiddleOfTyping = false
+        }
+        if let mathmaticalSymbol = sender.currentTitle{
+            brain.performOperation(mathmaticalSymbol)
+            
+        }
+        if let result = brain.result {
+            displayValue = brain.result!
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
